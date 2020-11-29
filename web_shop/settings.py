b5521d083776630c5da0177bff5334aab7f19350
Thor-Ole Øby <punkthor@gmail.com>
2020-11-29 19:40:51 +0100

    Edit settings

--- web_shop/settings.py
+++ web_shop/settings.py
@@ -21,7 +21,6 @@ if os.path.exists("web_shop/.env"):
 else:
     development = os.environ.get("DEVELOPMENT", False)
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
