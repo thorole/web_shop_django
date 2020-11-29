2d71fdba9e3e482a6ff262804cc614b7d0c18586
Thor-Ole Øby <punkthor@gmail.com>
2020-11-29 22:13:37 +0100

    Sending emails

--- web_shop/settings.py
+++ web_shop/settings.py
@@ -116,7 +116,6 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
@@ -222,10 +221,18 @@ STRIPE_CURRENCY = 'usd'
 DEFAULT_FROM_EMAIL = 'webshop@example.com'
 
 if development:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     STRIPE_PUBLIC_KEY = env('STRIPE_PUBLIC_KEY')
     STRIPE_SECRET_KEY = env('STRIPE_SECRET_KEY')
     STRIPE_WH_SECRET = env('STRIPE_WH_SECRET')
 else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_USER = os.environ.get('EMAIL_HOST_USER')
     STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
     STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
     STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET')
